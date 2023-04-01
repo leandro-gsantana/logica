5cd7050271b76b3c6e9473e6177a9ab6bd83ec9a
Leandro Santana <santana.turbo@gmail.com>
2023-04-01 14:47:58 -0300

    Exercicio Pronto

--- Exercicios/Ex01.rb
+++ Exercicios/Ex01.rb
@@ -1,6 +1,13 @@
 #
+puts 
+puts "********** Inicio *********"
+puts 
 puts "Informe um numero"
-num = gets
-soma = num -1
-
-puts "O numero digitado é: " + num + "seu antecessor é: " + soma
+num = gets.to_i
+soma = num - 1
+puts
+puts "*************************"
+puts
+puts "O numero digitado é: #{num}. " + "O seu antecessor é: #{soma}"
+puts
+puts "********** Fim *********"
